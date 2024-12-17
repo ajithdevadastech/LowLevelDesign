952caa1a5089fd339106e59bfadf553587af86b6
ajithdevadas <ajithdevadastech@gmail.com>
2024-12-17 14:05:54 -0500

    12-17-2024

--- Abstraction.py
+++ Abstraction.py
@@ -1,4 +1,15 @@
-class Circle:
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
         self.pi = 3.14
@@ -9,7 +20,23 @@ class Circle:
     def perimeter(self):
         return 2 * self.pi * self.radius
 
+class Rectangle(Shape):
+    def __init__(self, length, breadth):
+        self.length  = length
+        self.breadth = breadth
+
+    def area(self):
+        return self.length * self.breadth
+
+    def perimeter(self):
+        return 2 * (self.length + self.breadth)
+
+
 
 o = Circle(6)
 print(o.area())
 print(o.perimeter())
+
+p = Rectangle(3,4)
+print(p.area())
+print(p.perimeter())
